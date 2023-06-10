453eadd3dde30442458cb9d98b4bd1cd9c464388
Longthanh202 <thanhlongle978@gmail.com>
2023-06-10 22:48:59 +0700

    first commit

---
+++ Nhom01_CongNgheNet_/Nhom01_CongNghe.Net/frmQLPhong.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace Nhom01_CongNghe.Net
+{
+    partial class frmQLPhong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cboLoaiPhong = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTenPhong = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.dgvDanhSachPhong = new System.Windows.Forms.DataGridView();
+            this.MAPHONG = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TENPHONG = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TENLOAIPHONG = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDanhSachPhong)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cboLoaiPhong);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtTenPhong);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1288, 103);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin phòng";
+            // 
+            // cboLoaiPhong
+            // 
+            this.cboLoaiPhong.FormattingEnabled = true;
+            this.cboLoaiPhong.Location = new System.Drawing.Point(930, 55);
+            this.cboLoaiPhong.Name = "cboLoaiPhong";
+            this.cboLoaiPhong.Size = new System.Drawing.Size(114, 30);
+            this.cboLoaiPhong.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(815, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 22);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Loại phòng:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(405, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 22);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Tên phòng:";
+            // 
+            // txtTenPhong
+            // 
+            this.txtTenPhong.Location = new System.Drawing.Point(503, 56);
+            this.txtTenPhong.Name = "txtTenPhong";
+            this.txtTenPhong.Size = new System.Drawing.Size(86, 30);
+            this.txtTenPhong.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnXoa);
+            this.panel1.Controls.Add(this.btnSua);
+            this.panel1.Controls.Add(this.btnThem);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 579);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1288, 84);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnXoa.BackColor = System.Drawing.Color.Red;
+            this.btnXoa.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+            this.btnXoa.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.btnXoa.Location = new System.Drawing.Point(731, -4);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(154, 43);
+            this.btnXoa.TabIndex = 6;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = false;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnSua.BackColor = System.Drawing.Color.Red;
+            this.btnSua.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+            this.btnSua.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.btnSua.Location = new System.Drawing.Point(553, -4);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(154, 43);
+            this.btnSua.TabIndex = 7;
+            this.btnSua.Text = "Sữa";
+            this.btnSua.UseVisualStyleBackColor = false;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnThem.BackColor = System.Drawing.Color.Red;
+            this.btnThem.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+            this.btnThem.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.btnThem.Location = new System.Drawing.Point(377, -4);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(154, 43);
+            this.btnThem.TabIndex = 9;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = false;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // dgvDanhSachPhong
+            // 
+            this.dgvDanhSachPhong.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDanhSachPhong.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MAPHONG,
+            this.TENPHONG,
+            this.TENLOAIPHONG});
+            this.dgvDanhSachPhong.Location = new System.Drawing.Point(162, 109);
+            this.dgvDanhSachPhong.Name = "dgvDanhSachPhong";
+            this.dgvDanhSachPhong.RowTemplate.Height = 24;
+            this.dgvDanhSachPhong.Size = new System.Drawing.Size(941, 464);
+            this.dgvDanhSachPhong.TabIndex = 4;
+            this.dgvDanhSachPhong.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDanhSachPhong_CellClick);
+            // 
+            // MAPHONG
+            // 
+            this.MAPHONG.DataPropertyName = "MAPHONG";
+            this.MAPHONG.HeaderText = "Mã Phòng";
+            this.MAPHONG.Name = "MAPHONG";
+            this.MAPHONG.Width = 300;
+            // 
+            // TENPHONG
+            // 
+            this.TENPHONG.DataPropertyName = "TENPHONG";
+            this.TENPHONG.HeaderText = "Tên Phòng";
+            this.TENPHONG.Name = "TENPHONG";
+            this.TENPHONG.Width = 300;
+            // 
+            // TENLOAIPHONG
+            // 
+            this.TENLOAIPHONG.DataPropertyName = "TENLOAIPHONG";
+            this.TENLOAIPHONG.HeaderText = "Loại Phòng";
+            this.TENLOAIPHONG.Name = "TENLOAIPHONG";
+            this.TENLOAIPHONG.Width = 300;
+            // 
+            // frmQLPhong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1288, 663);
+            this.Controls.Add(this.dgvDanhSachPhong);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmQLPhong";
+            this.Text = "frmQLPhong";
+            this.Load += new System.EventHandler(this.frmQLPhong_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDanhSachPhong)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTenPhong;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.ComboBox cboLoaiPhong;
+        private System.Windows.Forms.DataGridView dgvDanhSachPhong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MAPHONG;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TENPHONG;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TENLOAIPHONG;
+    }
+}
---
+++ Nhom01_CongNgheNet_/Nhom01_CongNghe.Net/frmQLPhong.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Nhom01_CongNghe.Net.DAO;
+using Nhom01_CongNghe.Net.DTO;
+
+namespace Nhom01_CongNghe.Net
+{
+    public partial class frmQLPhong : Form
+    {
+        //dulieu dl = new dulieu();
+        //List<Phong> ds_Phong = new List<Phong>();
+        //List<Phong> ds_PhongSua = new List<Phong>();
+        //List<Phong> ds_PhongThem = new List<Phong>();
+        //List<Phong> ds_PhongXoa = new List<Phong>();
+        int id;
+
+        //List<LoaiPhong> ds_LoaiPhong = new List<LoaiPhong>();
+
+        DBConnect conn = new DBConnect();
+        SqlDataAdapter ada;
+        DataColumn[] primary = new DataColumn[1];
+        public frmQLPhong()
+        {
+            InitializeComponent();
+            //btnLuu.Visible = false;
+            //btnThem.Visible = false;
+            //btnXoa.Visible = false;
+            //btnSua.Visible = false;
+            //groupBox1.Visible = false;
+            Load_Data();
+            load_LoaiPhong();
+            loadPhong();
+            
+        }
+        private void LoadLoaiPhong()
+        {
+            //DataTable tb_LoaiPhong = dl.LoadLoaiPhong_DT();
+            //if (tb_LoaiPhong.Rows.Count > 0 && tb_LoaiPhong != null)
+            //{
+            //    foreach (DataRow dr in tb_LoaiPhong.Rows)
+            //    {
+            //        LoaiPhong lp = new LoaiPhong();
+            //        lp.MaLoaiPhong = dr[0].ToString();
+            //        lp.TenLoaiPhong = dr[1].ToString();
+            //        lp.DonGia = dr[2].ToString();
+            //        ds_LoaiPhong.Add(lp);
+            //    }
+            //}
+
+
+        }
+        public void Load_Data()
+        {
+            string query = "select * from PHONG";
+            ada = conn.getDataAdapter(query, "PHONG");
+            primary[0] = conn.DSet.Tables["PHONG"].Columns[0];
+            conn.DSet.Tables["PHONG"].PrimaryKey = primary;
+        }
+        public int LayMaxMa()
+        {
+            string query = "select max(MAPHONG) from PHONG";
+            int max = conn.TimMax(query);
+            return max;
+        }
+        //public void ds_LoaiPhong()
+        //{
+        //    string query = "select * from LOAIPHONG";
+        //    DataTable tb = conn.ExecuteDataTable(query);
+        //    cbbLoaiPhong.DisplayMember = "TENLOAIPHONG";
+        //    cbbLoaiPhong.ValueMember = "MALOAIPHONG";
+        //    cbbLoaiPhong.DataSource = tb;
+        //}
+        //private void Load_ListViewLoaiPhong()
+        //{
+        //    if(ds_LoaiPhong.Count>0)
+        //    {
+        //        lv_LoaiPhong.Items.Clear();
+        //        foreach(LoaiPhong lp in ds_LoaiPhong)
+        //        {
+        //            ListViewItem item = new ListViewItem(lp.MaLoaiPhong);
+        //            item.SubItems.Add(lp.TenLoaiPhong);
+        //            item.SubItems.Add(lp.DonGia);
+        //            lv_LoaiPhong.Items.Add(item);
+
+        //        }
+        //    }
+        //}
+        //private void LoadListView()
+        //{
+        //    DataTable dt_Phong = dl.LoadPhong();
+        //    if(dt_Phong.Rows.Count>0 && dt_Phong!=null)
+        //    {
+        //        foreach(DataRow dr in dt_Phong.Rows)
+        //        {
+        //            Phong p = new Phong();
+        //            p.MaPhong = int.Parse(dr[0].ToString());
+        //            p.TenPhong = dr[1].ToString();
+        //            p.TrangThai = int.Parse(dr[2].ToString());
+        //            p.MaLoaiPhong = int.Parse(dr[3].ToString());
+        //            ds_Phong.Add(p);
+        //        }
+        //    }
+        //}
+        private void LoadCbbLoaiPhong()
+        {
+            //DataSet ds_LoaiPhong = dl.LoadLoaiPhong();
+            //cboLoaiPhong.DataSource = ds_LoaiPhong.Tables["LOAIPHONG"];
+            //cboLoaiPhong.DisplayMember = "TENLOAIPHONG";
+            //cboLoaiPhong.ValueMember = "MALOAIPHONG";
+        }
+        //private void LoadListView_Phong()
+        //{
+        //    if(ds_Phong.Count>0)
+        //    {
+        //        lv.Items.Clear();
+        //        foreach(Phong p in ds_Phong)
+        //        {
+        //            ListViewItem item = new ListViewItem(p.MaPhong.ToString());
+        //            item.SubItems.Add(p.TenPhong);
+        //            item.SubItems.Add(p.TrangThai.ToString());
+        //            item.SubItems.Add(p.MaLoaiPhong.ToString());
+        //            lv.Items.Add(item);
+        //        }
+        //    }
+        //}
+
+        //private void lv_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+        //    if(lv.SelectedItems.Count==0)
+        //    {
+        //        return;
+        //    }
+        //    else
+        //    {
+        //        ListViewItem item = lv.SelectedItems[0];
+        //        txtMaPhong.Text = item.SubItems[0].Text;
+        //        txtTenPhong.Text = item.SubItems[1].Text;
+        //        txtTrangThai.Text = item.SubItems[2].Text;
+        //        cboLoaiPhong.SelectedValue = item.SubItems[3].Text;
+        //    }
+        //}
+
+        private void frmQLPhong_Load(object sender, EventArgs e)
+        {
+            //LoadListView();
+            //LoadCbbLoaiPhong();
+            //LoadListView_Phong();
+
+            //LoadLoaiPhong();
+            //Load_ListViewLoaiPhong();
+
+        }
+        void loadPhong()
+        {
+            string query = "SELECT MAPHONG, TENPHONG, LOAIPHONG.TENLOAIPHONG FROM PHONG, LOAIPHONG WHERE PHONG.LOAIPHONG = LOAIPHONG.MALOAIPHONG";
+            DataTable tb = conn.ExecuteDataTable(query);
+            dgvDanhSachPhong.DataSource = tb;
+        }
+        public void load_LoaiPhong()
+        {
+            string query = "select * from LOAIPHONG";
+            DataTable tb = conn.ExecuteDataTable(query);
+            cboLoaiPhong.DisplayMember = "TENLOAIPHONG";
+            cboLoaiPhong.ValueMember = "MALOAIPHONG";
+            cboLoaiPhong.DataSource = tb;
+        }
+        private void lv_LoaiPhong_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //if (lv_LoaiPhong.SelectedItems.Count == 0)
+            //{
+            //    return;
+            //}
+            //else
+            //{
+            //    ListViewItem item = lv_LoaiPhong.SelectedItems[0];
+            //    txtMaLoaiPhong.Text = item.SubItems[0].Text;
+            //    txtTenLoaiPhong.Text = item.SubItems[1].Text;
+            //    txtDonGia.Text = item.SubItems[2].Text;
+            //}
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            //LoadListView_Phong();
+            //lv.Clear();
+            
+            int ma = LayMaxMa() + 1;
+            try
+            {
+                if (txtTenPhong.Text != "")
+                {
+                    DataRow r = conn.DSet.Tables["PHONG"].NewRow();
+                    r["MAPHONG"] = ma;
+                    r["TENPHONG"] = txtTenPhong.Text;
+                    r["TRANGTHAI"] = 0;
+                    r["LOAIPHONG"] = cboLoaiPhong.SelectedValue;
+                    conn.DSet.Tables["PHONG"].Rows.Add(r);
+                    SqlCommandBuilder cmdbd = new SqlCommandBuilder(ada);
+                    ada.Update(conn.DSet, "PHONG");
+                    MessageBox.Show("Thêm thành công");
+                    loadPhong();
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Ban chua nhap ten phong");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Them that bai");
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtTenPhong.Text == "")
+                {
+                    MessageBox.Show("Ban hay chon phong muon xoa");
+                }
+                else
+                {
+                    DataRow r = conn.DSet.Tables["PHONG"].Rows.Find(id);
+                    if (r != null)
+                    {
+                        r.Delete();
+                    }
+                    SqlCommandBuilder cmdbd = new SqlCommandBuilder(ada);
+                    ada.Update(conn.DSet, "PHONG");
+                    MessageBox.Show("Xoa thành công");
+                    loadPhong();
+                 
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Xoa that bai");
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtTenPhong.Text == "")
+                {
+                    MessageBox.Show("Ban hay chon phong muon sua");
+                }
+                else
+                {
+                    DataRow r = conn.DSet.Tables["PHONG"].Rows.Find(id);
+                    if (r != null)
+                    {
+                        r["TENPHONG"] = txtTenPhong.Text;
+                        r["LOAIPHONG"] = cboLoaiPhong.SelectedValue;
+                    }
+                    SqlCommandBuilder cmdbd = new SqlCommandBuilder(ada);
+                    ada.Update(conn.DSet, "PHONG");
+                    MessageBox.Show("Sua thành công");
+                    loadPhong();            
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Sua that bai");
+            }
+        }
+         
+        private void dgvDanhSachPhong_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+            int i;
+                
+                i = dgvDanhSachPhong.CurrentRow.Index;
+                id = int.Parse(dgvDanhSachPhong.Rows[i].Cells["MAPHONG"].Value.ToString());
+                txtTenPhong.Text = dgvDanhSachPhong.Rows[i].Cells["TENPHONG"].Value.ToString();
+                cboLoaiPhong.Text = dgvDanhSachPhong.Rows[i].Cells["LOAIPHONG"].Value.ToString();
+                loadPhong();
+            }
+            catch
+            {
+                //MessageBox.Show("Lỗi!");
+            }
+        }
+    }
+}
